562951192f68c0239a6d854c87babee5cba7478c
siiiidddd <sidannurulkalifa08@gmail.com>
2024-05-26 18:52:56 +0700

    Add files via upload

--- streamlit_file.py
+++ streamlit_file.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
 st.write('hello world')
-anak = st.text_input("Masukan nama anak=")
-ortu = st.text_input("Masukan nama orang tua=")
+
 umur = st.number_input("Masukan umur anak= ", 0)
 tinggi = st.number_input("Masukan tinggi anak=", 0)
 berat = st.number_input("Masukan berat anak=", 0)
