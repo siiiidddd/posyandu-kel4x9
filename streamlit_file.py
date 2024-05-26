be80287a026feb452fd491b89aeecbe36350621e
siiiidddd <sidannurulkalifa08@gmail.com>
2024-05-26 20:11:34 +0700

    Add files via upload

--- streamlit_file.py
+++ streamlit_file.py
@@ -1,7 +1,8 @@
 import streamlit as st
+from PIL import image
 
-st.image('./c:\Users\User\Downloads\foto posyandu.jpeg.png')
-st.header('Posyandu Balita Bunda')
+st.image('foto posyandu.png')
+st.title('Posyandu Balita Bunda')
 st.write("Keterangan")
 
 umur = st.number_input("Masukan umur anak= ", 0)
@@ -9,8 +10,8 @@ tinggi = st.number_input("Masukan tinggi anak=", 0)
 berat = st.number_input("Masukan berat anak=", 0)
 selanjutnya = st.button("lanjut")
 if selanjutnya :
-    berat < 3
+    berat < 10
     st.write("kurang gizi")
 else selanjutnya :
-    berat > 3
+    berat > 10
     st.write("cukup")
