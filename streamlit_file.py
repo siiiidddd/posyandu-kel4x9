80a6602da00564470ee2b136159210d87e8a7838
siiiidddd <sidannurulkalifa08@gmail.com>
2024-05-26 19:12:30 +0700

    Add files via upload

--- streamlit_file.py
+++ streamlit_file.py
@@ -1,11 +1,15 @@
 import streamlit as st
 
-st.write('hello world')
+st.header('Posyandu Balita Bunda')
+st.write("Keterangan")
 
 umur = st.number_input("Masukan umur anak= ", 0)
 tinggi = st.number_input("Masukan tinggi anak=", 0)
 berat = st.number_input("Masukan berat anak=", 0)
 selanjutnya = st.button("lanjut")
 if selanjutnya :
-    sidan = 1 * umur
-    st.write("Umur=",sidan)
+    berat < 3
+    st.write("kurang gizi")
+elif selanjutnya :
+    berat > 3
+    st.write("cukup")
