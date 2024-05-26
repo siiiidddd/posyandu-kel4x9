e47b07d039d37b00c0283aaf3a2825515eb2ab6f
siiiidddd <sidannurulkalifa08@gmail.com>
2024-05-26 18:48:32 +0700

    Add files via upload

--- streamlit_file.py
+++ streamlit_file.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
 st.write('hello world')
-umur = st.number_input("Masukan umur= ", 0)
-usia = st.button("lanjut")
-if usia :
+anak = st.text_input("Masukan nama anak=")
+ortu = st.text_input("Masukan nama orang tua=")
+umur = st.number_input("Masukan umur anak= ", 0)
+tinggi = st.number_input("Masukan tinggi anak=", 0)
+berat = st.number_input("Masukan berat anak=", 0)
+selanjutnya = st.button("lanjut")
+if selanjutnya :
     sidan = 1 * umur
     st.write("Umur=",sidan)
