52104f11b4489d8631623ad072915a0e410ab6d5
siiiidddd <sidannurulkalifa08@gmail.com>
2024-05-26 16:31:38 +0700

    Add files via upload

--- streamlit_file.py
+++ streamlit_file.py
@@ -3,5 +3,7 @@ import streamlit as st
 st.write('hello world')
 umur = st.number_input("Masukan umur= ", 0)
 usia = st.button("lanjut")
+
 if usia :
-st.write("Umur=",umur)
+sidan = umur
+st.write("Umur=",sidan)
