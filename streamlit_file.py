d01edd0e497eafa4648e211f2956cc7c8b3559dc
siiiidddd <sidannurulkalifa08@gmail.com>
2024-05-27 18:49:52 +0700

    Add files via upload

--- streamlit_file.py
+++ streamlit_file.py
@@ -6,8 +6,10 @@ st.write("Keterangan")
 umur = st.number_input("Masukan umur anak= ", 0)
 tinggi = st.number_input("Masukan tinggi anak=", 0)
 berat = st.number_input("Masukan berat anak=", 0)
-selanjutnya = st.button("lanjut")
-if selanjutnya :
-    umur == 1
+
+if umur == 1
     berat < 3
     st.write("kurang gizi")
+elif umur == 1
+    berat > 5
+    st.write("Lebih gizi")
