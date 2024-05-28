d3c53742245373c869d19b1c826b20ba0d9fd10c
siiiidddd <sidannurulkalifa08@gmail.com>
2024-05-28 09:19:08 +0700

    Add files via upload

--- streamlit_file.py
+++ streamlit_file.py
@@ -15,8 +15,24 @@ if umur == 1 :
 elif umur == 1 :
     berat > 5
 elif umur == 2 :
-    berat < 5
+    berat < 4
     st.write("Kurang gizi")
 elif umur == 2 :
+    berat > 6
+elif umur == 3 :
+    berat < 4
+    st.write("Kurang gizi")
+elif umur == 3 :
     berat > 7
+elif umur == 4 :
+    berat < 5
+    st.write("Kurang gizi")
+elif umur == 4 :
+    berat > 7.5
+elif umur == 5 :
+    berat < 6
+    st.write("Kurang gizi")
+elif umur == 5 :
+    berat > 8
+
     
