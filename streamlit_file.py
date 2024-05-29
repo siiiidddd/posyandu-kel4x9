6231a81704d69326ef50dd7c3ffe2b7ba512894d
siiiidddd <sidannurulkalifa08@gmail.com>
2024-05-29 12:47:25 +0700

    Add files via upload

--- streamlit_file.py
+++ streamlit_file.py
@@ -8,30 +8,58 @@ st.write("Pendaftaran")
 st.write("Pengukuran dan Penimbangan")
 st.write("Pencatatan")
 st.write("Penyuluhan Kesehatan")
-st.write("Jika tertulis keterangan 'true' berarti kurang gizi, jika tertulis 'false' maka gizi tidak kurang")
 
 umur = st.number_input("Masukan umur anak(bulan)= ", 0)
-tinggi = st.number_input("Masukan tinggi anak=", 0)
 berat = st.number_input("Masukan berat anak=", 0)
 lanjut = st.button("Selanjutnya")
 
-if umur == 1  and berat < 3:
+if lanjut and umur == 1  and berat < 3:
     st.write("kurang gizi")
-elif umur == 1 and berat > 5:
+elif lanjut and umur == 1 and berat > 5:
     st.write("gizi lebih")
-elif umur == 2 and berat < 4:
+elif lanjut and umur == 2 and berat < 4:
     st.write("Kurang gizi")
-elif umur == 2 and berat > 6:
+elif lanjut and umur == 2 and berat > 6:
     st.write("gizi lebih")
-elif umur == 3 and berat < 4:
+elif lanjut and umur == 3 and berat < 4:
     st.write("Kurang gizi")
-elif umur == 3 and berat > 7:
+elif lanjut and umur == 3 and berat > 7:
     st.write("gizi lebih")
-elif umur == 4 and berat < 5:
+elif lanjut and umur == 4 and berat < 5:
     st.write("Kurang gizi")
-elif umur == 4 and berat > 7.5:
+elif lanjut and umur == 4 and berat > 7.5:
     st.write("gizi lebih")
-elif umur == 5 and berat < 6:
+elif lanjut and umur == 5 and berat < 6:
     st.write("Kurang gizi")
-elif umur == 5 and berat > 8:
+elif lanjut and umur == 5 and berat > 8:
     st.write("gizi lebih")
+elif lanjut and umur == 6 and berat < 6:
+    st.write("kurang gizi")
+elif lanjut and umur == 6 berat > 8:
+     st.write("gizi lebih")
+elif lanjut and umur == 7 and berat < 6:
+     st.write("kurang gizi")
+elif lanjut and umur == 7 and berat > 9:
+    st.write("gizi lebih")
+elif lanjut and umur == 8 and berat < 6.5:
+    st.write("kurang gizi")
+elif lanjut and umur == 8 and berat > 9:
+    st.write("gizi lebih")
+elif lanjut and umur == 9 and berat < 7:
+    st.write("kurang gizi")
+elif lanjut and umur == 9 and berat > 9: 
+     st.write("gizi lebih")
+elif lanjut and umur == 10 and berat < 7:
+    st.write("kurang gizi")
+elif lanjut and umur == 10 and berat > 10:
+    st.write("gizi lebih")
+elif lanjut and umur == 11 and berat < 7:
+     st.write("kurang gizi")
+elif lanjut and umur == 11 and berat > 10:
+     st.write("gizi lebih")
+elif lanjut and umur == 12 and berat < 7
+     st.write("kurang gizi")
+elif lanjut and umur == 12 and berat > 10.5
+     st.write("gizi lebih")
+else :
+    st.write("Gizi cukup")
